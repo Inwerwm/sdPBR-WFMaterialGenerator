ff42c11e1fb8eff9b6ccd44896e77c59ed912201
Inwerwm <saclainvervm@gmail.com>
2021-07-24 11:24:38 +0900

    wfマテリアル生成処理を実装

--- sdPBR-WFMaterialGenerator/WFMaterialGenerator.cs
+++ sdPBR-WFMaterialGenerator/WFMaterialGenerator.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace sdPBR_WFMaterialGenerator
 {
     public class WFMaterialGenerator
     {
+        private string GenerateMaterialString(string targetFilePath) =>
+            $"#define IN_THE_MIRROR{Environment.NewLine}" +
+            $"#include \"{Path.GetFileName(targetFilePath)}\"";
+
+        private void WriteFile(string outputFilename, string materialString)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            File.WriteAllText(outputFilename, materialString, Encoding.GetEncoding("shift_jis"));
+        }
+
+        public void GenerateWFMaterial(string targetFilePath)
+        {
+            var targetDir = Path.GetDirectoryName(targetFilePath);
+            var targetFilename = Path.GetFileName(targetFilePath);
+
+            WriteFile(Path.Combine(targetDir, "wf_" + targetFilename), GenerateMaterialString(targetFilePath));
+        }
     }
 }
