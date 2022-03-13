5d3db50647b1405cd4b967cbb30ee7c41e4a56e1
Inwerwm <saclainvervm@gmail.com>
2022-03-13 16:50:40 +0900

    引数の数判定を厳しくした

--- wfMaterialGenerator-Console/Program.cs
+++ wfMaterialGenerator-Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using sdPBR_WFMaterialGenerator;
 using System;
 
-if (args.Length < 2)
+if (args.Length != 2)
     Console.WriteLine("usage: wfMaterialGenerator-Console [source material directory] [destination material directory]");
 else
     new WFMaterialGenerator(args[0], args[1]).Generate();
