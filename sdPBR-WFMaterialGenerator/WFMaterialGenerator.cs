3471259486be88b71231b4ed7752a5ec3f3b4658
Inwerwm <saclainvervm@gmail.com>
2022-03-13 16:31:56 +0900

    出力物生成処理を実装

--- sdPBR-WFMaterialGenerator/WFMaterialGenerator.cs
+++ sdPBR-WFMaterialGenerator/WFMaterialGenerator.cs
@@ -4,35 +4,67 @@ using System.Text;
 
 namespace sdPBR_WFMaterialGenerator
 {
-    public static class WFMaterialGenerator
+    public class WFMaterialGenerator
     {
-        private static void WriteFile(string outputPath, string materialString)
+        private string SourceDirectory { get; }
+        private string DestinationDirectory { get; }
+
+        public WFMaterialGenerator(string sourceDirectory, string destinationDirectory)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            File.WriteAllText(outputPath, materialString, Encoding.GetEncoding("shift_jis"));
+            if (!IsDirectory(sourceDirectory))
+            {
+                throw new ArgumentException("作成元材質フォルダとして指定されたフォルダが見つかりませんでした。");
+            }
+
+            if (!IsDirectory(destinationDirectory))
+            {
+                throw new ArgumentException("出力先材質フォルダとして指定されたフォルダが見つかりませんでした。");
+            }
+
+            SourceDirectory = sourceDirectory;
+            DestinationDirectory = destinationDirectory;
+        }
+
+        public void Generate()
+        {
+            
+        }
+
+        private IEnumerable<(string OutputPath, string Contents)> CreateMaterials(string path)
+           => IsDirectory(path) ? GetChildItems(path).SelectMany(CreateMaterials)
+            : IsEffectFile(path) ? Return(CreateWFMaterial(path))
+            : Enumerable.Empty<(string OutputPath, string Contents)>();
+
+        private bool IsEffectFile(string path) => Path.GetExtension(path).ToLower() == ".fx";
+
+        private (string OutputPath, string Contents) CreateWFMaterial(string path)
+        {
+            var sourceRelativePath = Path.GetRelativePath(SourceDirectory, path);
+            var destinationPath = Path.Combine(DestinationDirectory, sourceRelativePath);
+
+            return (destinationPath, GenerateMaterialString(path));
+        }
+
+        private static IEnumerable<T> Return<T>(T value)
+        {
+            yield return value;
+        }
+
+        private static bool IsDirectory(string path) => Directory.Exists(path);
+
+        private static IEnumerable<string> GetChildItems(string path)
+        {
+            return Directory.GetFiles(path).Concat(Directory.GetDirectories(path));
         }
 
         private static string GenerateMaterialString(string sourceFilePath) =>
             $"#define IN_THE_MIRROR{Environment.NewLine}" +
             $"#include \"{Path.GetFileName(sourceFilePath)}\"";
 
-        private static bool IsDirectory(string path) => Directory.Exists(path);
-        private static bool IsFile(string path) => File.Exists(path);
-
-        public static void Generate(string sourceMaterialDirectory, string destinationMaterialDirectory)
+        private static void WriteFile(string outputPath, string materialString)
         {
-            if(!IsDirectory(sourceMaterialDirectory))
-            {
-                throw new ArgumentException("作成元材質フォルダとして指定されたフォルダが見つかりませんでした。");
-            }
-
-            if (!IsDirectory(destinationMaterialDirectory))
-            {
-                throw new ArgumentException("出力先材質フォルダとして指定されたフォルダが見つかりませんでした。");
-            }
-
-
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            File.WriteAllText(outputPath, materialString, Encoding.GetEncoding("shift_jis"));
         }
-
     }
 }
