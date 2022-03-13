cbe6e449d29dc3172522494bf3efaca6a66c1c08
Inwerwm <saclainvervm@gmail.com>
2022-03-13 16:44:33 +0900

    起動処理を更新

--- wfMaterialGenerator-Console/Program.cs
+++ wfMaterialGenerator-Console/Program.cs
@@ -1,47 +1,7 @@
 ﻿using sdPBR_WFMaterialGenerator;
 using System;
-using System.IO;
-using System.Linq;
 
-namespace wfMaterialGenerator_Console
-{
-    class Program
-    {
-        private static WFMaterialGenerator Generator { get; } = new();
-        private static uint GenerationCount { get; set; } = 0;
-
-        static void Main(string[] args)
-        {
-            foreach (var arg in args)
-            {
-                Generate(arg);
-            }
-
-            Console.WriteLine($"完了{Environment.NewLine}{GenerationCount}個のファイルを生成しました。");
-        }
-
-        static void Generate(string path)
-        {
-            bool isEffectFile = Path.GetExtension(path).ToLower() == ".fx";
-            bool isWFMaterial = Path.GetFileName(path).ToLower().StartsWith("wf_");
-
-            if (IsFile(path) && isEffectFile && !isWFMaterial)
-            {
-                Generator.GenerateWFMaterial(path);
-                GenerationCount++;
-            }
-            else if(IsDirectory(path))
-            {
-                var childrenOfDir = Directory.GetFiles(path).Concat(Directory.GetDirectories(path));
-                
-                foreach (var childItem in childrenOfDir)
-                {
-                    Generate(childItem);
-                }
-            }
-        }
-
-        static bool IsDirectory(string path) => Directory.Exists(path);
-        static bool IsFile(string path) => File.Exists(path);
-    }
-}
+if (args.Length < 2)
+    Console.WriteLine("usage: wfMaterialGenerator-Console [source material directory] [destination material directory]");
+else
+    new WFMaterialGenerator(args[0], args[1]).Generate();
